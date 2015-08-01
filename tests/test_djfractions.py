dca3a40c483e9cb4e246e37152fb7bddc0404db7
Justin Michalicek <jmichalicek@gmail.com>
2015-08-01 21:10:12 +0000

    A few more tests cases for display_fraction
    * Test mixed numbers
    * Make a main template instance variable for the display_fraction tests
    * Started a test case for DecimalFractionField but it has no tests yet

--- tests/test_djfractions.py
+++ tests/test_djfractions.py
@@ -4,6 +4,7 @@ from django.template import Template, Context
 from decimal import Decimal
 
 from djfractions import quantity_to_decimal
+from djfractions.forms import DecimalFractionField
 
 
 class QuantityToDecimalTest(TestCase):
@@ -44,23 +45,39 @@ class DisplayFractionTagTest(TestCase):
     Test the quantity_to_decimal() function
     """
 
-    def test_whole_number(self):
-        template = Template("""
+    def setUp(self):
+        self.template = Template("""
         {% load fractions %}
-        {% display_fraction 1 %}
+        {% display_fraction frac %}
         """)
-        c = Context({})
 
-        rendered = template.render(c)
+        self.template_reduce_false = Template("""
+        {% load fractions %}
+        {% display_fraction frac False %}
+        """)
+
+
+    def test_whole_number(self):
+        c = Context({'frac': 1})
+        rendered = self.template.render(c)
         self.assertEqual(rendered.strip(), '1')
 
     def test_simple_fraction(self):
-        template = Template("""
-        {% load fractions %}
-        {% display_fraction .5 %}
-        """)
-        c = Context({})
+        c = Context({'frac': .5})
 
-        rendered = template.render(c)
+        rendered = self.template.render(c)
         self.assertEqual(rendered.strip(), '<sup>1</sup>&frasl;<sub>2</sub>')
 
+        c = Context({'frac': Decimal('.5')})
+        rendered = self.template.render(c)
+        self.assertEqual(rendered.strip(), '<sup>1</sup>&frasl;<sub>2</sub>')
+
+
+    def test_complex_number(self):
+        c = Context({'frac': 1.5})
+        rendered = self.template.render(c)
+        self.assertEqual(rendered.strip(), '1 <sup>1</sup>&frasl;<sub>2</sub>')
+
+
+class DecimealFractionFieldTest(TestCase):
+    pass
