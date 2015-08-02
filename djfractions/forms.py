149706b9f6ade1b7383e368bd524cae092bb435d
Justin Michalicek <jmichalicek@gmail.com>
2015-08-02 03:05:36 +0000

    Tests for DecimalFractionField
    * Lots of tests for DecimalFractinField, several bug fixes
    * Removal of pointless code
    * Improved error checking

--- djfractions/forms.py
+++ djfractions/forms.py
@@ -1,4 +1,4 @@
-from __future__ import unicode_literals, absolute_import
+from __future__ import unicode_literals, absolute_import, division
 
 from django import forms
 from django.core.exceptions import ValidationError
@@ -9,16 +9,13 @@ from decimal import Decimal
 import fractions
 import re
 
-from . import quantity_to_decimal
+from . import quantity_to_decimal, is_number
 
 
 class DecimalFractionField(forms.Field):
     """
     Displays and takes input as a fraction string such as 1/4 or 1 1/4
     but stores as a Decimal.
-
-    This may be updated to handle float, allow specifying the
-    decimal params, etc.
     """
 
     default_error_messages = {
@@ -27,22 +24,25 @@ class DecimalFractionField(forms.Field):
 
     def __init__(self, *args, **kwargs):
         """
-        Takes optional param `coerce_thirds`, which defaults to True.  If True
-        then .3 repeating is forced to 1/3 rather than 3/10, 33/100, etc.
-        and .66 and .67 are forced to 2/3.
+        :param bool coerce_thirds: Defaults to True.  If True
+            then .3 repeating is forced to 1/3 rather than 3/10, 33/100, etc.
+            and .66 and .67 are forced to 2/3.
+        :param int limit_denominator: Set a maximum denominator to be used on
+            fractions created from the field input.
         """
         self.coerce_thirds = kwargs.pop('coerce_thirds', True)
-        super(FractionField, self).__init__(*args, **kwargs)
+        self.limit_denominator = kwargs.pop('limit_denominator', None)
+        super(DecimalFractionField, self).__init__(*args, **kwargs)
 
     def prepare_value(self, value):
-        if isinstance(value, Decimal):
+        if isinstance(value, Decimal) or isinstance(value, float):
             f = fractions.Fraction(value)
             dec = Decimal(value).quantize(Decimal('0.00'))
 
             if f.denominator == 1:
                 return str(f)
-            elif dec == Decimal('.33') or dec == Decimal('.3') or dec == Decimal('.67') or dec == Decimal('.6'):
-                f = f.limit_denominator(3)
+            #elif dec == Decimal('.33') or dec == Decimal('.3') or dec == Decimal('.67') or dec == Decimal('.6'):
+            #    f = f.limit_denominator(3)
 
             fraction_string = ''
             if f.numerator > f.denominator:
@@ -51,44 +51,41 @@ class DecimalFractionField(forms.Field):
                 f = fractions.Fraction(f.numerator - (int_part * f.denominator), f.denominator)
                 fraction_string = u'%d' % int_part
 
-            gcd = fractions.gcd(f.numerator, f.denominator)
-            # reduce to lowest terms
-            f = f/gcd
+            if self.limit_denominator:
+                f = f.limit_denominator(self.limit_denominator)
 
             if self.coerce_thirds:
                 temp_decimal = Decimal(f.numerator / f.denominator).quantize(Decimal('0.00'))
-                if temp_decimal == Decimal('.33') or temp_decimal == Decimal('.3') or \
-                   temp_decimal == Decimal('.67') or temp_decimal == Decimal('.6'):
+                if temp_decimal % 1 == Decimal('.33') or temp_decimal % 1 == Decimal('.3') \
+                   or temp_decimal % 1 == Decimal('.67') or temp_decimal % 1 == Decimal('.6'):
                     f = f.limit_denominator(3)
 
-            fraction_string = '%s %d/%d' % (fraction_string, f.numerator, f.denominator)
+            fraction_string = u'%s %d/%d' % (fraction_string, f.numerator, f.denominator)
         else:
-            fraction_string = value
+            fraction_string = u'%s' % value
         return fraction_string.strip()
 
     def to_python(self, value):
         """
         Take string input such as 1/4 or 1 1/3 and convert to a :class:`decimal.Decimal`
         """
-        # if this works, I should move quantity_to_decimal into the core app
-        from recipes import quantity_to_decimal
-
         if value in EMPTY_VALUES:
             return None
 
-        if isinstance(value, Decimal):
-            return value
+        if isinstance(value, Decimal) or isinstance(value, float) or isinstance(value, int):
+            return Decimal(value)
 
         # some really lame validation that we do not have a string like "1 1 1/4" because that
         # is not a valid number.
-        # these regexes should match 1 1/4 and 1/4, with any number of spaces between digits and /
-        # and any length of actual digits such as 100 1/4 or 1 100/400, etc
-        try:
-            int(value)
-            is_int = True
-        except ValueError:
-            is_int = False
-        if not is_int and not re.match(r'^\s*\d+\s*\/\s*\d+\s*$', value) and not re.match(r'^\s*\d+\s+\d+\s*\/\s*\d+$\s*', value):
+        # these regexes should match fractions such as 1 1/4 and 1/4, with any number
+        # of spaces between digits and / and any length of actual digits such as
+        # 100 1/4 or 1 100/400, etc
+        if not is_number(value) and not re.match(r'^\s*\d+\s*\/\s*\d+\s*$', value) \
+           and not re.match(r'^\s*\d+(\s+|\s+and\s+|\s*\-\s*)\d+\s*\/\s*\d+$\s*', value):
+            # this second matches optional whitespace, then a digit, then
+            # whitespace OR the word 'and' with or without spaces OR a hyphen with
+            # or without surrounding spaces, followed by another digit, a /, then a digit
+            # examples: 1 1/2, 1-1/2, 1 - 1/2, 1 and 1/2, etc.
             raise ValidationError(self.error_messages['invalid'], code='invalid')
 
         try:
@@ -98,7 +95,7 @@ class DecimalFractionField(forms.Field):
         return value
 
     def validate(self, value):
-        super(FractionField, self).validate(value)
+        super(DecimalFractionField, self).validate(value)
         if value in self.empty_values:
             return
         # Check for NaN, Inf and -Inf values. We can't compare directly for NaN,
--- tests/test_djfractions.py
+++ tests/test_djfractions.py
@@ -1,3 +1,5 @@
+from __future__ import division, absolute_import, unicode_literals
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from django.template import Template, Context
 
@@ -80,4 +82,172 @@ class DisplayFractionTagTest(TestCase):
 
 
 class DecimealFractionFieldTest(TestCase):
-    pass
+
+    def test_prepare_value_int(self):
+        """
+        Test that a standard int as input is returned
+        as a string of that int, so `1` is returned as `'1'`
+        """
+        field = DecimalFractionField()
+        result = field.prepare_value(1)
+        self.assertEqual('1', result)
+
+    def test_prepare_value_string(self):
+        """
+        Test string fractions are returns as is
+        """
+        field = DecimalFractionField()
+        result = field.prepare_value('1/4')
+        self.assertEqual('1/4', result)
+
+        result = field.prepare_value('1 1/4')
+        self.assertEqual('1 1/4', result)
+
+    def test_prepare_value_decimal(self):
+        """
+        Test that a :class:`decimal.Decimal` is properly
+        converted to a string fraction
+        """
+        field = DecimalFractionField()
+        result = field.prepare_value(Decimal('.5'))
+        self.assertEqual('1/2', result)
+
+    def test_prepare_value_float(self):
+        """
+        Test that a :class:`float` is properly
+        converted to a string fraction
+        """
+        field = DecimalFractionField()
+        result = field.prepare_value(float(.5))
+        self.assertEqual('1/2', result)
+
+    def test_prepare_value_limit_denominator(self):
+        """
+        Test `prepare_value()` when the field has been initialized
+        with the limit_denominator paramter
+        """
+        field = DecimalFractionField(limit_denominator=3)
+        result = field.prepare_value(Decimal(1/3.0))
+        self.assertEqual('1/3', result)
+
+    def test_prepare_value_coerce_thirds(self):
+        """
+        Test that when coerce_thirds is specified, then .66, .67, and .33, etc.
+        are converted properly to 1/3 and 2/3
+        """
+        field = DecimalFractionField(coerce_thirds=True)
+        result = field.prepare_value(Decimal(1/3.0))
+        self.assertEqual('1/3', result)
+
+        result = field.prepare_value(Decimal(1/3.0))
+        self.assertEqual('1/3', result)
+
+        result = field.prepare_value(Decimal(2/3.0))
+        self.assertEqual('2/3', result)
+
+        result = field.prepare_value(Decimal(2/6.0))
+        self.assertEqual('1/3', result)
+
+        result = field.prepare_value(Decimal(4/6.0))
+        self.assertEqual('2/3', result)
+
+        result = field.prepare_value(Decimal(4/3.0))
+        self.assertEqual('1 1/3', result)
+
+        result = field.prepare_value(Decimal(5/3.0))
+        self.assertEqual('1 2/3', result)
+
+    def test_to_python_decimal(self):
+        """
+        Test that whena :class:`decimal.Decimal` is passed to to_python()
+        the value is returned as is
+        """
+        field = DecimalFractionField()
+        value = Decimal(.5)
+        result = field.to_python(value)
+        self.assertEqual(value, result)
+
+    def test_to_python_float(self):
+        """
+        Test that whena :class:`float` is passed to to_python()
+        the value is returned as as :class:`decimal.Decimal`
+        """
+        field = DecimalFractionField()
+        value = .5
+        result = field.to_python(value)
+        self.assertEqual(Decimal(value), result)
+
+    def test_to_python_int(self):
+        """
+        Test that whena :class:`int` is passed to to_python()
+        the value is returned as as :class:`decimal.Decimal`
+        """
+        field = DecimalFractionField()
+        value = 1
+        result = field.to_python(value)
+        self.assertEqual(Decimal(value), result)
+
+    def test_to_python_int_string(self):
+        field = DecimalFractionField()
+        value = '2'
+        result = field.to_python(value)
+        self.assertEqual(Decimal('2'), result)
+
+    def test_to_python_float_string(self):
+        field = DecimalFractionField()
+        value = '0.5'
+        result = field.to_python(value)
+        self.assertEqual(Decimal(value), result)
+
+    def test_to_python_fraction_string(self):
+        field = DecimalFractionField()
+        value = '1/2'
+        result = field.to_python(value)
+        self.assertEqual(Decimal('.5'), result)
+
+    def test_to_python_mixed_fraction_string(self):
+        field = DecimalFractionField()
+        value = '1 1/2'
+        result = field.to_python(value)
+        self.assertEqual(Decimal(3/2.0).quantize(Decimal('0.000')),
+                         result.quantize(Decimal('0.000')))
+
+    def test_to_python_hyphenated_mixed_fraction_string(self):
+        field = DecimalFractionField()
+        value = '1-1/2'
+        result = field.to_python(value)
+        self.assertEqual(Decimal(3/2.0).quantize(Decimal('0.000')),
+                         result.quantize(Decimal('0.000')))
+
+        value = '1 - 1/2'
+        result = field.to_python(value)
+        self.assertEqual(Decimal(3/2.0).quantize(Decimal('0.000')),
+                         result.quantize(Decimal('0.000')))
+
+    def test_to_python_anded_mixed_fraction_string(self):
+        field = DecimalFractionField()
+        value = '1 and 1/2'
+        result = field.to_python(value)
+        self.assertEqual(Decimal(3/2.0).quantize(Decimal('0.000')),
+                         result.quantize(Decimal('0.000')))
+
+    def test_to_python_validation_errors(self):
+        field = DecimalFractionField()
+        with self.assertRaises(ValidationError):
+            field.to_python('abcd')
+
+        with self.assertRaises(ValidationError):
+            field.to_python('1 1 1/3')
+
+        with self.assertRaises(ValidationError):
+            field.to_python('1 1')
+
+    def test_validate_inf_raises_error(self):
+        field = DecimalFractionField()
+        with self.assertRaises(ValidationError):
+            field.validate(Decimal("Inf"))
+
+    def test_validate_negative_inf_raises_error(self):
+        field = DecimalFractionField()
+        with self.assertRaises(ValidationError):
+            field.validate(Decimal("-Inf"))
