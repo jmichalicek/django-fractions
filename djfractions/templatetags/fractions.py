be2ff552c91f67cf362aed0554dfe733fe9300e6
Justin Michalicek <jmichalicek@gmail.com>
2015-08-05 01:54:04 +0000

    New display_improper_fraction template tag.
    * New template tag display_improper_fraction which always shows
      improper fractions for whole numbers and fractions greater than 1.

--- djfractions/templatetags/fractions.py
+++ djfractions/templatetags/fractions.py
@@ -49,3 +49,23 @@ def display_fraction(value, limit_denominator=None, allow_mixed_numbers=True,
         fraction_string = u'%s' % value
 
     return mark_safe(fraction_string.strip())
+
+
+@register.simple_tag(name='display_improper_fraction')
+def display_improper_fraction(value, limit_denominator=None, coerce_thirds=True):
+    """
+    Display a numeric value as an html fraction using
+    <sup>numerator</sup>&frasl;<sub>denominator</sub>.
+    This tag will never convert to single whole numbers or
+    to mixed numbers, it will return improper fractions such as 3/2
+    or even 4/1
+
+    :param bool limit_denominator: Limit the denominator to this value.  Defaults to None,
+        which in effect results in the :meth:`fractions.Fraction.limit_denominator()`
+        default of `1000000`
+    :param bool coerce_thirds:  If True then .3 repeating is forced to 1/3
+        rather than 3/10, 33/100, etc. and .66 and .67 are forced to 2/3.
+        Defaults to True.
+    """
+    return display_fraction(value, limit_denominator=limit_denominator,
+                            allow_mixed_numbers=False, coerce_thirds=coerce_thirds)
