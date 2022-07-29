7007390ee43daa496ff9903c0695c6c16aa0da35
Justin Michalicek <jmichalicek@gmail.com>
2022-07-29 21:35:16 +0000

    Django 4.1 prep updates
    * Dropped testing support for django<3.2, added testing of django 4.1
    * Updated type hints for DecimalFractionField.formfield
    * Added AppConfig for djfractions
    * Bumped python version in dev dockerfile to 3.10.5
    * Added vscode configs to .gitignore

---
+++ djfractions/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FractionsAppConfig(AppConfig):
+    name = 'djfractions'
--- test_project/frac/frac/settings.py
+++ test_project/frac/frac/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app.apps.AppConfig',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -106,3 +106,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
